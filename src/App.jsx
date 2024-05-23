c08c7d763e2039e61d65d6af487a9f3a8d429f18
gerar <gerar132000ng@gmail.com>
2024-05-23 10:08:46 -0500

    add:feat

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+  return(
+    <div className="h-screen flex justify-center items-center dark:bg-neutral-900">
+      <button className="bg-slate-200 px-4 py-2 rounded hover:bg-slate-300 dark:bg-slate-950 dark:text-white dark:hover:bg-slate-900" >
+        Change theme
+      </button>
+    </div>
+  )
+}
+export default App
